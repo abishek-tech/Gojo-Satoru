029daabb95b24ea9b5763d69c91851b931c1c5af
abishek-tech <85898840+abishek-tech@users.noreply.github.com>
2022-07-28 23:18:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['91**********']
-global.premium = ['91**********']
-global.ownernomer = '91**********'
+global.owner = ['1(606) 441-1808']
+global.premium = ['1(606) 441-1808']
+global.ownernomer = '1(606) 441-1808'
 global.ownername = '𝑺𝑶𝑼𝑳 𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑹'
 global.botname = '𝑫𝑶𝑴𝑰𝑵𝑨𝑻𝑶𝑻 𝑺𝑬𝑹 𝑩𝑶𝑻'
 global.footer = '©sᴇʀᴅᴏᴍɪɴᴀᴛᴏʀ'
